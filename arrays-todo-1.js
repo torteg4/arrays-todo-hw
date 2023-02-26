275cd9de8eb5eff57a1d1ffa4a944584a30e858c
Tiffany O <tiffanyortega29@gmail.com>
2023-02-25 20:33:30 -0600

    updated with explanations

--- arrays-todo-1.js
+++ arrays-todo-1.js
@@ -27,6 +27,21 @@
         arr3.splice(2, 0, 311);
             //? The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. var.splice(1, 0, 'Feb') => Inserts at index 1 => output: Array ["Jan", "Feb", "March", "April", "June"]
         console.log("INSERT AT:", arr3);
-        // console.log("POP FRONT Removed Variable:", removedVar);
-            // POP FRONT: [ 5, 10, 15 ]
-            // POP FRONT Removed Variable: 0
+
+//* BONUS: Remove At
+    
+    // removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
+    var arr4 = [1000,3,204,77];
+    var splicedVar = arr4.splice(1,1);
+        //? You can remove the element at any index by using the splice method. arr.splice(n, 1), with n being the index of the element to remove.
+        console.log("REMOVE AT:", arr4);
+        console.log("REMOVE AT Spliced Variable:", splicedVar);
+
+//* BONUS: Swap Pairs - incomplete
+    
+    // insertAt(["Brendan",true,42]) => [true,"Brendan",42]
+    var arr5 = ["Brendan", true, 42];
+    var splicedVar = arr5.swap(1,1);
+        //? You can remove the element at any index by using the splice method. arr.splice(n, 1), with n being the index of the element to remove.
+    //     console.log("REMOVE AT:", arr4);
+    //     console.log("REMOVE AT Spliced Variable:", splicedVar);
